3107fe891563b94d922b29ab66ae4817f4a37ee0
Owusu Bonsu <38985725+OwusuBonsu@users.noreply.github.com>
2021-05-29 15:34:47 -0400

    Various site updates

--- src/App.js
+++ src/App.js
@@ -1,22 +1,24 @@
-import logo from './Me.png';
-import './App.css'
-import ReactFloaterJs from 'react-floaterjs'
-
+import logo from "./Me.png";
+import "./App.css";
+import ReactFloaterJs from "react-floaterjs";
 
 function App() {
   return (
-    <div className="App"> 
-    <ReactFloaterJs>
-    <div className="Pic">
-      <img src={logo} className="Me" alt="logo" />
-    </div>
-    </ReactFloaterJs>
+    <div className="App">
+      <ReactFloaterJs>
+        <div className="Pic">
+          <img src={logo} className="Me" alt="logo" />
+        </div>
+      </ReactFloaterJs>
 
-    <div className="Info">
-      <h1> Owusu Bonsu's Portfolio </h1>
-      <p> idk what to put here yet lmao </p>
-    </div>
-
+      <div className="Info">
+        <h1> Owusu Bonsu's Portfolio </h1>
+        <p>
+          {" "}
+          Hi 👋🏿 Owusu here. I'm an aspiring front-end web developer. This is my
+          website. Browse around to learn more about me.{" "}
+        </p>
+      </div>
     </div>
   );
 }
--- src/Projects.js
+++ src/Projects.js
@@ -1,11 +1,27 @@
-import React from 'react';
+import React from "react";
+import "./Projects.css";
+import PAM from "./PlaylistAnalystMockup.png";
+import Button from "react-bootstrap/Button";
 
-function Projects(){
-    return (
-        <div className="Projects">
-        <p>Projects go here</p>
-        </div>
-        )
+function Projects() {
+  return (
+    <div className="Projects">
+      <div className="Proj1img">
+        <img src={PAM} />
+      </div>
+      <div className="Proj1Desc">
+        <h2>The Spotify Playlist Analyst</h2>A tool designed to give you a
+        breakdown of your spotify playlists, telling you the artists, albums and
+        genres they're comprised of.
+        <Button
+          variant="dark"
+          href="https://spotify-playlist-analyst.herokuapp.com"
+        >
+          Go
+        </Button>
+      </div>
+    </div>
+  );
 }
 
 export default Projects;

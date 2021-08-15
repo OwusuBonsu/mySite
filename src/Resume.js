787436277dc4eb62ecfd00967dc6495a36eb37ed
Owusu Bonsu <owusu.bonsu10@gmail.com>
2021-08-14 23:15:48 -0400

    Resume page fixes

--- src/Resume.js
+++ src/Resume.js
@@ -1,7 +1,9 @@
 import file from "./OwusuBonsu_Resume.pdf";
+import img from "./OwusuBonsu_Resume-1.jpg";
 import "./resume.css";
 import React from "react";
 import { Document, Page, pdfjs } from "react-pdf";
+import Card from "react-bootstrap/Card";
 
 pdfjs.GlobalWorkerOptions.workerSrc = `//cdnjs.cloudflare.com/ajax/libs/pdf.js/${pdfjs.version}/pdf.worker.js`;
 function Resume() {
@@ -10,11 +12,17 @@ function Resume() {
       <div className="Header">
         <h1>My Resume</h1>
       </div>
-      <div className="pageHolder">
-        <Document file={file} renderMode="svg">
-          <Page pageNumber={1} renderTextLayer={false} />
-        </Document>
-      </div>
+      <Card
+        className="m-3 resumeCard"
+        style={{ backgroundColor: `rgb(18, 18, 18)` }}
+      >
+        <div className="pageHolder">
+          <img src={img} className="resumeIMG" />
+          <Document className="resumeSVG" file={file} renderMode="svg">
+            <Page pageNumber={1} renderTextLayer={false} />
+          </Document>
+        </div>
+      </Card>
     </>
   );
 }

3457d2a24e688ca461cd0c758c4f883138db64c7
Owusu Bonsu <owusu.bonsu10@gmail.com>
2021-09-17 19:05:53 -0400

    Add todo project

--- src/Projects.js
+++ src/Projects.js
@@ -1,5 +1,6 @@
 import React from "react";
 import PAM from "./PlaylistAnalystMockup.png";
+import TDM from "./ToDoMockup.png";
 import "bootstrap/dist/css/bootstrap.min.css";
 import ProjectTemplate from "./ProjectTemplate";
 
@@ -9,7 +10,7 @@ function Projects() {
       <div className="Header">
         <h1>My Projects</h1>
       </div>
-      <div className="Projects">
+      <div className="Projects mb-5">
         <ProjectTemplate
           image={PAM}
           name="The Spotify Playlist Analyst"
@@ -24,6 +25,19 @@ function Projects() {
             "Async Programming",
             "React Hooks",
             "Nivo",
+          ]}
+        />
+        <ProjectTemplate
+          image={TDM}
+          name="Public To-Do List"
+          desc="To be honest... I don't know what the purpose of this is. It's a simple To-Do list that takes advantage of Firebase's Realtime Database in order to sync to-do items and their status'"
+          link="https://brave-franklin-d211ca.netlify.app/"
+          tags={[
+            "React",
+            "Firebase",
+            "Material UI",
+            "React Hooks",
+            "Realtime Updates",
           ]}
         />
       </div>

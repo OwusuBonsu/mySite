cd76f739fe4623d95e05ec5b54f115d51b7d667d
Owusu Bonsu <owusu.bonsu10@gmail.com>
2021-10-18 12:02:03 -0400

    Resume card fixes

--- src/Resume.js
+++ src/Resume.js
@@ -4,6 +4,7 @@ import "./resume.css";
 import React from "react";
 import { Document, Page, pdfjs } from "react-pdf";
 import Card from "react-bootstrap/Card";
+import "react-pdf/dist/esm/Page/AnnotationLayer.css";
 
 pdfjs.GlobalWorkerOptions.workerSrc = `//cdnjs.cloudflare.com/ajax/libs/pdf.js/${pdfjs.version}/pdf.worker.js`;
 function Resume() {
@@ -19,7 +20,7 @@ function Resume() {
         <div className="pageHolder">
           <img src={img} className="resumeIMG" />
           <Document className="resumeSVG" file={file} renderMode="svg">
-            <Page pageNumber={1} renderTextLayer={false} />
+            <Page pageNumber={1} renderTextLayer={true} />
           </Document>
         </div>
       </Card>
